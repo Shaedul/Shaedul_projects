6994591c422b153d06152a2ee6e00e8721331e92
Shaedul <45014392+Shaedul@users.noreply.github.com>
2018-11-19 00:13:45 +0600

    Add files via upload

--- Blood Bank Maintenance/index.php
+++ Blood Bank Maintenance/index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Blood bank Management System</title>
+<title>Untitled Document</title>
 <link href="css/lightbox.css" rel="stylesheet" />
     <link href="StyleSheet.css" rel="stylesheet" type="text/css" />
 
@@ -47,64 +47,32 @@
 </div>
 <div class="nav_bg">
 <div class="wrap">
-		<?php require('header.php');?>
+		<ul class="nav">
+			<li class="active"><a href="chngpwd.php">Change Password</a></li>	
+			<li><a href="updateprofile.php">Update Profile</a></li>            
+			<li><a href="blooddonated.php">Blood Donated</a></li>
+            <li><a href="viewdonations.php">View Donations</a></li>
+            <li><a href="viewrequest.php">View Requestes</a></li>
+            <li><a href="logout.php">log Out</a></li>
+           
+            </ul>
 	</div>
-
-        <div>
-
-          <div class="h_btm_bg">
-<div class="wrap">
-<div class="h_btm">
-  
-	
-	
-<div class="main">
-	<div class="content">	
-    	
-		<center>	<h4><u>Welcome to our Blood bank</u></h4></center>
-			<p align="justify">We welcome you to in our WebSite. If you are a donor , We appreciate you <a href="registration.php">signing up</a> online as a Donor. If you need blood we are happy to serve you. This blood donor list is hosted by <a href="index.php">Website</a> on behalf of "Online Blood Bank"
-as a public service without any profit motive.This is a free service. While the Organisers have taken all steps to obtain accurate and up-to-date information of potential blood donors, 
-the Organisers and ICM Computers do not guarantee accuracy of the information contained herein or the suitability of the persons listed as any liability for direct or consequential damage to any person using this blood donor list including 
-loss of life or damage due to infection of any nature arising out of blood transfusion from persons whose names have been listed in this website.
-We request donors to update contact details regularly.</p>
-	</div>
-	<div class="sidebar">
-		<center>	<h2>Latest News</h2></center>
-			<div class="blog_posts">
-
-    <marquee direction="up" scrolldelay="300"><table >
-<tr><td>
-                         <div class="blog_desc">
-						<div class="blog_heading">
-						</div>	
-                         <br />
-	<span style="font-weight:bold"><b>World Blood Donor Day: Poor health reduces women blood donors in Chittagong</b>
-<p>While the overall number of blood donors in Chittagong has grown over the years, women constitute only a tiny 10 percent share owing to health problems like pernicious anaemia and low haemoglobin levels or being underweight</p></span>
-					  </div>
-                        </td></tr>
-                   
-                </table></marquee>  
-				 
-					 <div class="clear"></div>	
-				</div>	
+<div style="height:300px; width:700px; margin:auto; margin-top:50px; margin-bottom:50px; background-color:#f8f1e4; border:2px solid red; box-shadow:4px 1px 20px black;">
+    <img src="Images/donorpannel.png"/>
+        
 			
 			
 	</div>
-	<div class="clear"></div>
-	</div>
-</div>
-</div>
-</div>
+	
 
-        </div>
-        <div class="clear"></div>
+      <div class="clear"></div>
 <div class="ftr-bg">
 <div class="wrap">
 <div class="footer">
 	<div class="f_nav">
 		<ul>
 			<li class="active"><a href="index.php">Home</a></li>			
-			<li><a href="donar.php">Donor</a></li>
+			<li><a href="donor.php">Donor</a></li>
             <li><a href="login.php">log In</a></li>
             <li><a href="aboutus.php">About</a></li>
             <li><a href="contact.php">Contact Us</a></li>
@@ -112,7 +80,7 @@ We request donors to update contact details regularly.</p>
             </ul>
 	</div>
 		<div class="copy">
-			<p class="title">Shahed© All Rights Reserved </p>
+			<p class="title">© All Rights Reserved | Design by Mr. Bhatia |</p>
 		</div>
 	<div class="clear"></div>
 </div>
